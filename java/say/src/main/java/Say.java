a83ed3bcfcc258240caaadc92f3052e5ed2a672d
Alec Tunbridge <alec.tunbridge@autotrader.co.uk>
2026-05-21 17:10:51 +0100

    Refactor 1

--- java/say/src/main/java/Say.java
+++ java/say/src/main/java/Say.java
@@ -1,7 +1,7 @@
 public class Say {
 
     public static final String[] ONES = new String[]{
-            "",
+            "zero",
             "one",
             "two",
             "three",
@@ -22,64 +22,45 @@ public class Say {
             "eighteen",
             "nineteen"
     };
-    private static final String[] TENS = {"", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    private static final String[] TENS = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
 
     public String say(long number) {
-        String result = "";
 
         if (number < 0 || number > 999_999_999_999L) {
             throw new IllegalArgumentException();
         }
 
-        if (number == 0) {
-            return "zero";
-        }
-
         if (number >= 1_000_000_000) {
-            result += say(number / 1000_000_000) + " billion";
-            long remainder = number % 1000_000_000;
-            if (remainder == 0) {
-                return result;
-            }
-            return result + " " + say(remainder);
+            return doUnit(number, 1_000_000_000, "billion");
         }
-        if (number >= 1000_000) {
-            result += say(number / 1000_000) + " million";
-            long remainder = number % 1000_000;
-            if (remainder == 0) {
-                return result;
-            }
-            return result + " " + say(remainder);
+        if (number >= 1_000_000) {
+            return doUnit(number, 1_000_000, "million");
         }
-        if (number >= 1000) {
-            result += say(number / 1000) + " thousand";
-            long remainder = number % 1000;
-            if (remainder == 0) {
-                return result;
-            }
-            return result + " " + say(remainder);
+        if (number >= 1_000) {
+            return doUnit(number, 1_000, "thousand");
         }
         if (number >= 100) {
-            result += ONES[((int) number) / 100] + " hundred";
-            long remainder = number % 100;
-            if (remainder == 0) {
-                return result;
-            }
-            return result + " " + say(remainder);
+            return doUnit(number, 100, "hundred");
         }
         if (number >= 20) {
-            result += TENS[((int) number) / 10 - 1];
+            String result = TENS[((int) number) / 10];
             long remainder = number % 10;
             if (remainder == 0) {
                 return result;
             }
             return result + "-" + say(number % 10);
         } else {
-            result += ONES[(int) number];
+            return ONES[(int) number];
         }
+    }
 
-
-        return result;
+    private String doUnit(long number, int unit, String unitAsText) {
+        String result = say(number / unit) + " " + unitAsText;
+        long remainder = number % unit;
+        if (remainder == 0) {
+            return result;
+        }
+        return result + " " + say(remainder);
     }
 }
